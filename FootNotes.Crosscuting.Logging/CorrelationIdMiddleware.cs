60cc5266def64c61e1291d6b570ff172828893be
Joao Augusto <joaootsugua@gmail.com>
2025-10-12 12:02:49 -0300

    Add logging configuration

---
+++ FootNotes.Crosscuting.Logging/CorrelationIdMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Serilog.Context;
+
+namespace FootNotes.Crosscuting.Logging
+{
+    public class CorrelationIdMiddleware(RequestDelegate next)
+    {
+        private const string CorrelationHeader = "X-Correlation-Id";
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string correlationId = context.Request.Headers.TryGetValue(CorrelationHeader, out StringValues value)
+                ? value.ToString()
+                : Guid.NewGuid().ToString();
+
+            
+            context.Response.Headers[CorrelationHeader] = correlationId;
+            LogContext.PushProperty("CorrelationId", correlationId);
+
+            await next(context);
+        }
+    }
+}
---
+++ FootNotes.Crosscuting.Logging/LoggingConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Formatting.Json;
+
+namespace FootNotes.Crosscuting.Logging
+{
+    public static class LoggingConfiguration
+    {
+        public static void Configure(ConfigurationManager configuration)
+        {
+            Log.Logger = new LoggerConfiguration()                
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .Enrich.WithEnvironmentName()
+                .Enrich.WithMachineName()
+                .Enrich.WithThreadId()
+                .Enrich.WithProcessId()
+                .WriteTo.Async(wa => wa.Console())
+                .WriteTo.Async(wa => wa.File(new JsonFormatter(), "logs/log-.txt", rollingInterval: RollingInterval.Day))                
+                .CreateLogger();
+        }
+    }
+}
